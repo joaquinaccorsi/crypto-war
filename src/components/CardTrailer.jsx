55b119f8d4bef26308f87c3b67b3f68a6cef0232
Joaquín Accorsi <joaquinaccorsi@gmail.com>
2022-01-25 16:39:41 -0300

    2ndEstructure

---
+++ src/components/CardTrailer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logofull from "../LogoCWFull.png";
+
+function CardTrailer() {
+    return ( 
+<div className='card'>
+    <div className='card-title'>
+     <h3>Se parte de <img src={logofull} className='Title-Logo'/></h3>
+    </div>
+    <div className='card-body'>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/DaKqP_HBp2M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>       </iframe>
+    </div>
+</div>
+ );
+
+}
+
+export default CardTrailer;
