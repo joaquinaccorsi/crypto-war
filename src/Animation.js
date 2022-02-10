977fbd49accb833fa526b564947a3ab3dc721375
Joaquín Accorsi <joaquinaccorsi@gmail.com>
2022-02-10 18:20:40 -0300

    BUILD AND DEPLOY A PRODUCCIÓN

--- src/Animation.js
+++
@@ -1,9 +0,0 @@
-let ScrollReveal;
-
-ScrollReveal().reveal(".Inicio",{delay: 200});
-ScrollReveal().reveal(".Game-Trailer",{delay: 500});
-ScrollReveal().reveal(".Game-Story",{delay: 750});
-ScrollReveal().reveal(".Game-Play",{delay: 1000});
-ScrollReveal().reveal(".Cofres",{delay: 1250});
-ScrollReveal().reveal(".Team",{delay: 1500});
-ScrollReveal().reveal(".Road-Map",{delay: 1750});
