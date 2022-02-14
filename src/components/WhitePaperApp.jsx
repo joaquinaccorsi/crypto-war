be6d19744655769950023a960727ec5969686ca2
Joaquín Accorsi <joaquinaccorsi@gmail.com>
2022-02-14 20:27:08 -0300

    update css

--- src/components/WhitePaperApp.jsx
+++ src/components/WhitePaperApp.jsx
@@ -21,12 +21,12 @@ function WhitePaperApp (){
       <div className="TituloWP">
         <img src={logofull}className="LogoWP"/>
         
-        <a href='https://www.instagram.com/cryptowaroficial/' target="_blank"><img src= {IconInstagram} className='IconInstagramWP'/></a>
-        <a href='https://discord.gg/s8uD7a6PyM'target="_blank"><img src= {IconDiscord} className='IconDiscordWP'/></a>
-        <a href='https://www.facebook.com/CryptoWar-100465312564842'target="_blank"><img src= {IconFacebook}className='IconFacebookWP'/></a>
-        <a href='https://twitter.com/CryptoWar_CW'target="_blank"><img src= {IconTwitter} className='IconTwitterWP'/></a>
-        <a href='https://www.linkedin.com/company/crypto-war'target="_blank"><img src={IconLinkedin} className='IconLinkedinWP'/></a>
-        <a href='https://t.me/CryptoWarOficial'target="_blank"><img src={IconTelegram} className='IconTelegramWP'/></a>
+        <a href='https://www.instagram.com/cryptowaroficial/' target="_blank"className='aWP'><img src= {IconInstagram} className='IconInstagramWP'/></a>
+        <a href='https://discord.gg/s8uD7a6PyM'target="_blank"className='aWP'><img src= {IconDiscord} className='IconDiscordWP'/></a>
+        <a href='https://www.facebook.com/CryptoWar-100465312564842'target="_blank"className='aWP'><img src= {IconFacebook}className='IconFacebookWP'/></a>
+        <a href='https://twitter.com/CryptoWar_CW'target="_blank"className='aWP'><img src= {IconTwitter} className='IconTwitterWP'/></a>
+        <a href='https://www.linkedin.com/company/crypto-war'target="_blank"className='aWP'><img src={IconLinkedin} className='IconLinkedinWP'/></a>
+        <a href='https://t.me/CryptoWarOficial'target="_blank"className='aWP'><img src={IconTelegram} className='IconTelegramWP'/></a>
         <a href="/" className="Atrás">VOLVER</a>
         
 
@@ -37,10 +37,10 @@ function WhitePaperApp (){
     
         <a href="/WhitePaper/Tokenomics"  className="LinkTokenomics">TOKENOMICS</a>
         <a href="/WhitePaper/Recompensas" className="LinkRecompensas">RECOMPENSAS</a>
-        <a href="/WhitePaper/Tokenomics"  className="LinkTokenomics">TOKENOMICS</a>
-        <a href="/WhitePaper/Recompensas" className="LinkRecompensas">RECOMPENSAS</a>
-        <a href="/WhitePaper/Tokenomics"  className="LinkTokenomics">TOKENOMICS</a>
-        <a href="/WhitePaper/Recompensas" className="LinkRecompensas">RECOMPENSAS</a>
+        <a href="/WhitePaper/Tokenomics"  className="LinkTokenomics">xxxxxx</a>
+        <a href="/WhitePaper/Recompensas" className="LinkRecompensas">xxxxxx</a>
+        <a href="/WhitePaper/Tokenomics"  className="LinkTokenomics">xxxxxx</a>
+        <a href="/WhitePaper/Recompensas" className="LinkRecompensas">xxxxxx</a>
 
       </div>
 
